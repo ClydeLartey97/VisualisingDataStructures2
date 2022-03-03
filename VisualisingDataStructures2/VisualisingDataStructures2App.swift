20853d04bd2f6fa754e37e34a47b7ebd72a5d742
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-03-03 11:55:31 +0000

    Initial Commit

---
+++ VisualisingDataStructures2/VisualisingDataStructures2App.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataStructures2App.swift
+//  VisualisingDataStructures2
+//
+//  Created by Lartey, David (HWTA) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataStructures2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
